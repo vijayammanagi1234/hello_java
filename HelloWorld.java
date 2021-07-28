99cfad45c51c984d7e8014c26f79cb375f26e827
EC2 Default User <ec2-user@ip-172-31-41-238.ap-south-1.compute.internal>
2021-07-28 14:35:40 +0000

    new added the job : added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World ,,,,,,,,,,,,,,,,,,,,,,,");
-    System.out.println ("this new job create here")
+    System.out.println ("this new job create here");
   
   
 
